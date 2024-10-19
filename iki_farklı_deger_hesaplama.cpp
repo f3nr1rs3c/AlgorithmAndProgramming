5f75f5d60c6eaf935bb80efecd76027aba5dc8e4
Doğukan İSPİRLİ <51451598+f3nr1rs3c@users.noreply.github.com>
2024-10-19 18:53:45 +0300

    Create iki_farklı_deger_hesaplama.cpp

---
+++ iki_farklı_deger_hesaplama.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int deger1, deger2;
+    cout<<"Deger1 giriniz: ";
+    cin>>deger1;
+    cout<<"Deger2 giriniz: ";
+    cin>>deger2;
+
+    cout<<"Toplama islemi sonucu = "<<deger1 + deger2<<endl;
+}
