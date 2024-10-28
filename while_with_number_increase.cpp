72368eab335aaf1a62956e44974b5d82a81dfea4
Doğukan İSPİRLİ <51451598+f3nr1rs3c@users.noreply.github.com>
2024-10-28 19:56:39 +0300

    Create while_with_number_increase.cpp

---
+++ while_with_number_increase.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    int i = 0; // i'ye sayısal değer olarak 0 tanımlattım.
+    while (i < 5) {
+        cout << i << "Kücüktür.";
+        i++;
+        // Çıktı olarak 5 kez kücüktür yazdırdık.
+    }
+}
