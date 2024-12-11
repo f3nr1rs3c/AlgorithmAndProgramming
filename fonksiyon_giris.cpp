472a75d8be90c926aa30747723de2750f9b2cc8b
Doğukan İSPİRLİ <51451598+f3nr1rs3c@users.noreply.github.com>
2024-12-11 18:46:19 +0300

    Create fonksiyon_giris.cpp

---
+++ fonksiyon_giris.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+void mesaj() { // Void int bölümündeki mesajları çekmiyor.
+	cout << "Yüzlerce mesaj 1" << endl;
+	cout << "Yüzlerce mesaj 2 " << endl;
+	cout << "Yüzlerce mesaj 3" << endl;
+	cout << "Yüzlerce mesaj 4" << endl;
+	cout << "Yüzlerce mesaj 5" << endl;
+}
+
+int main() {
+	mesaj();
+}
